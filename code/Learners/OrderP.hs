dfc779a4ac91745c1333d0beeca540a374e6c622
Aaron Shim <aaron.shim@yale.edu>
2016-04-19 19:14:18 -0400

    subtle error in OrderP... trying to fix it now

--- code/Learners/OrderP.hs
+++ code/Learners/OrderP.hs
@@ -29,6 +29,7 @@ instance Attribute (M.Map (IRLine,IRLine)) (Integer, Integer) where
    let
      relevantRules = M.filterWithKey (hasRuleFor f) (rs)
      fRules = learn f :: OrdMap (Integer, Integer)
+     -- ok, we really need to figure out why we have this second removeConflicts here...
      fRules' = M.foldrWithKey removeConflicts fRules fRules  :: OrdMap (Integer, Integer)
      diff = traceMe $ M.filterWithKey (\k v -> filterProbs cutoffProb v) $ relevantRules M.\\ fRules' --the difference between the two rule sets
      x = if M.null diff then Nothing else Just diff
@@ -40,11 +41,16 @@ instance Attribute (M.Map (IRLine,IRLine)) (Integer, Integer) where
   -- this is "ok" becuase odering merge does not satisfy the algorithm spec (see paper)
   merge curr new =
     let
-      u = M.union curr new
+      u = M.union curr new -- also I'm pretty sure this doesn't add counts for ones that do stack up
       x = M.foldrWithKey removeConflicts u u
-    in x
+    in
+      mergeWhileRemovingConflicts curr new M.empty
     --L.intersect curr new
 
+-- while this works for the non-counting version, running this more than once is iffy on the counting version
+--  (as well as across two merged sets) because every time it runs, we exponentially increase (double-ish) the
+--  counts on both the rule and its negation, and then do it again on the negation and its rule
+-- Ideally, let's restrict this to run exactly once during the learn step!
 removeConflicts :: (IRLine,IRLine) -> (Integer, Integer) -> OrdMap (Integer,Integer) -> OrdMap (Integer,Integer)
 removeConflicts k (vy, vn) old =
   let
@@ -52,9 +58,15 @@ removeConflicts k (vy, vn) old =
     conflict =  M.lookup flippedRule old
   in
     case conflict of -- can't delete because it'll screw up \\
+    -- we can only do this when one is from one set and the other is from the other set
+    --  (otherwise, if we keep running this on itself, we exponentially blow up counts)
       Just (y, n) -> M.insert k (vy + n, vn + y) $ M.insert flippedRule (y + vn, n + vy) old
       Nothing -> old -- no need for redundant insert?
 
+-- better version that looks across the merging maps and does not exponentially blow up
+--mergeWhileRemovingConflicts :: OrdMap (Integer,Integer) -> OrdMap (Integer,Integer) -> OrdMap (Integer, Integer) -> OrdMap (Integer, Integer)
+--mergeWhileRemovingConflicts (c:cs) ns seed =
+  -- is this even the right approach? Or should we not keep track of duplicates (a,b) and (b,a) and elect to keep just one copy that is perfectly invertible?
 
 
 
