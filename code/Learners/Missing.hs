5add6cb93188e121eea6855bf4d2ddafc2c24b9e
Mark Santolucito <mark.santolucito@yale.edu>
2016-08-02 14:55:50 -0400

    no more int rel learning for now
    it doenst work with json and we have a newer version from xinyu waiting to be integrated anyway

--- code/Learners/Missing.hs
+++ code/Learners/Missing.hs
@@ -14,22 +14,6 @@ import qualified Data.Map as M
 
 import Debug.Trace
 
-
-instance Attribute [] MissingKVRule where
-  learn [] = []
-  learn (l:ls) = concatMap (\l' -> if (keyword l == keyword l') then [] else [MissingKVRule l l']) ls ++ learn ls
-
-  check rs f =
-   let
-     fRules = learn f
-     diff = rs L.\\ fRules --the difference between the two rule sets
-     x = if null diff then Nothing else Just diff
-   in
-    x
-
-  merge curr new = L.intersect curr new
-
-
 instance Attribute [] MissingKRule where
   learn [] = []
   learn (l:ls) = concatMap (\l' -> if (keyword l == keyword l') then [] else [MissingKRule (keyword l) (keyword l')]) ls ++ learn ls
--- code/Types/Types.hs
+++ code/Types/Types.hs
@@ -52,24 +52,10 @@ type TypeMap a = M.Map Keyword a
 type OrdMap a = M.Map (IRLine,IRLine) a
 type IntRelMap = M.Map (Keyword,Keyword) (Maybe (Int->Int->Bool))
 
--- the types of these rules restricts the search space for learning modules
-type Formula = Maybe (Int -> Int -> Bool)
-data OrdRule = OrdRule {
-  ord_l1  :: IRLine,
-  ord_l2  :: IRLine,
-  ord_rel :: Bool} deriving (Show,Eq)
---you are missing a key-value pair correlation
-data MissingKVRule = MissingKVRule {
-  l11 ::IRLine,
-  l22 :: IRLine} deriving (Show,Eq, Generic,Data,Typeable, ToJSON, FromJSON)
 --you are missing a key correlation
 data MissingKRule = MissingKRule {
   k1 :: Keyword,
   k2 :: Keyword} deriving (Show,Eq, Generic, Data,Typeable,ToJSON, FromJSON)
-data IntRelRule = IntRelRule {
-  l1      :: Keyword,
-  l2      :: Keyword,
-  formula :: Maybe (Int->Int->Bool)} deriving (Show)
 -- a formula count, basically a triple of integers for instances of the pair
 --  with ordering <=, >=, or ==
 data FormulaC = FormulaC {
@@ -77,16 +63,3 @@ data FormulaC = FormulaC {
   gt :: Int,
   eq :: Int
   } deriving (Show, Eq, Generic, Data,Typeable,ToJSON, FromJSON)
-
-
-instance Show (Int-> Int -> Bool) where
-  show f1 =
-    if | (f1 0 1)  -> "<="
-       | (f1 1 0) -> ">="
-       | (f1 1 1) -> "=="
--- only for comparators! careful!
-instance Eq (Int -> Int -> Bool) where
- (==) f1 f2 =
-   (f1 1 1) == (f2 1 1) &&
-   (f1 0 1) == (f2 0 1) &&
-   (f1 1 0) == (f2 1 0)
