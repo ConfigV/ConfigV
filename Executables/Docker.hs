ed76582f07e20ab21daf34da4f570c645264420c
Mark Santolucito <mark.santolucito@yale.edu>
2018-12-19 09:51:41 -0400

    add docker runner

---
+++ Executables/Docker.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import           ConfigV
+import qualified GHC.IO.Encoding       as G
+import           System.IO
+import           System.Exit
+
+main = do
+  G.setLocaleEncoding utf8
+  G.setFileSystemEncoding utf8
+  G.setForeignEncoding utf8  
+ 
+  executeLearning settings (Left csvThresholds)
+  actualResults   <- readFile cachedRulesDefaultLoc
+ 
+  putStrLn actualResults
+
+settings = learnConfig {
+        learnTarget = "docker-configv/"
+      , enableMissing = True
+      , verbose = True
+      }
+
+csvThresholds = defaultThresholds {
+        keywordCoorSupport = 10
+      , keywordCoorConfidence = 1
+      }
+
+
