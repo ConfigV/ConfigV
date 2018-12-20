cb3a705c59a2bb4881a4d0707a42ea30dcd7b1cd
Mark Santolucito <mark.santolucito@yale.edu>
2018-12-19 22:16:20 -0500

    add docker results

--- Executables/Docker.hs
+++ Executables/Docker.hs
@@ -12,18 +12,21 @@ main = do
  
   executeLearning settings (Left csvThresholds)
   actualResults   <- readFile cachedRulesDefaultLoc
- 
-  putStrLn actualResults
+  return ()
+--  putStrLn actualResults
 
 settings = learnConfig {
         learnTarget = "docker-configv/"
       , enableMissing = True
+      , enableOrder = True
       , verbose = True
       }
 
 csvThresholds = defaultThresholds {
-        keywordCoorSupport = 10
+        keywordCoorSupport = 12
       , keywordCoorConfidence = 1
+      , orderSupport = 12
+      , orderConfidence = 1 
       }
 
 
