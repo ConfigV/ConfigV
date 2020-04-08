9dffc17f43ab1489bbad57c84226f70675080882
Mark Santolucito <mark.santolucito@yale.edu>
2020-04-07 21:09:51 -0400

    new unit tests for impl graph

--- Tests/UnitTests.hs
+++ Tests/UnitTests.hs
@@ -7,19 +7,49 @@ import System.Exit
 import Control.Exception
 
 import ConfigV.Learners.Common
-import ConfigV.Types.IR
+import ConfigV.Types
+import ConfigV.BuildImplGraph
 
+import qualified Data.Map as M
 import qualified Data.Set as S
 
-ir1 = IRLine{keyword="k1", value="a"}
-ir2 = IRLine{keyword="k2", value="b"}
-
+-- | are pairs constructed correctly
 pairTest :: TestTree
 pairTest = testCase "pair test" $ 
   (pairsAsSet $ S.fromList [ir1,ir2]) @?= 
   (S.fromList $ [(ir1,ir2), (ir2,ir1)])
+ir1 = IRLine{keyword="k1", value="a"}
+ir2 = IRLine{keyword="k2", value="b"}
 
-main = defaultMain pairTest
+
+-- | can an implication graph be rendered without crashing
+implicationGraphRender :: TestTree
+implicationGraphRender = testGroup "impl graph tests" [implGraphEmpty, implGraphOne]
+
+-- | render an empty graph
+implGraphEmpty :: TestTree
+implGraphEmpty = testCase "render empty impl graph" $
+  implGraph $ M.empty
+
+implGraphTiny:: TestTree
+implGraphTiny = testCase "render tiny impl graph" $
+  implGraph $ M.fromList [(simpleSmtFormula, simpleAntiRule), (simpleSmtFormula2, simpleAntiRule)]
+simpleSmtFormula = SMTFormula {
+         antecedent = STrue
+       , consequent = STrue
+       }
+simpleSmtFormula2 = SMTFormula {
+         antecedent = And STrue STrue
+       , consequent = STrue
+       }
+simpleAntiRule = AntiRule {
+   tru = 1
+  ,fls = 0
+  ,tot = 1
+  } 
+
+
+main = defaultMain (testGroup "unit tests" [pairTest, implicationGraphRender])
   `catch` (\e -> do
     if e == ExitSuccess
       then putStrLn "Yea"
